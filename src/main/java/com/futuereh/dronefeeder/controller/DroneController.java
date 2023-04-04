0a30c63eebe9e18bc2abd03e0cf711b9c4ff34ed
Ana Luisa <ana.luisa.isa@hotmail.com>
2023-04-04 18:05:36 -0300

    feat(controller/drone): Implemetação do método inserirDrone utilizando PostMapping

--- src/main/java/com/futuereh/dronefeeder/controller/DroneController.java
+++ src/main/java/com/futuereh/dronefeeder/controller/DroneController.java
@@ -1,7 +1,11 @@
 package com.futuereh.dronefeeder.controller;
 
+import com.futuereh.dronefeeder.model.Drone;
 import com.futuereh.dronefeeder.service.DroneService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,4 +17,8 @@ public class DroneController {
   @Autowired
   private DroneService droneService;
 
+  @PostMapping
+  public ResponseEntity<Drone> inserirDrone(@RequestBody Drone drone) {
+    return ResponseEntity.ok().body(droneService.inserirDrone(drone));
+  }
 }
