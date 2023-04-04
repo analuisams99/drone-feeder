088a70525c0a9e7fab434e0c5441aaae947eba7b
Ana Luisa <ana.luisa.isa@hotmail.com>
2023-04-04 17:30:37 -0300

    feat(service): Criação da classe DroneService e implemetação do método inserirDrone injetando DroneRepository

---
+++ src/main/java/com/futuereh/dronefeeder/service/DroneService.java
@@ -0,0 +1,19 @@
+package com.futuereh.dronefeeder.service;
+
+import com.futuereh.dronefeeder.model.Drone;
+import com.futuereh.dronefeeder.repository.DroneRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**Classe de serviço DroneService.*/
+@Service
+public class DroneService {
+  @Autowired
+  private DroneRepository repository;
+  
+  /**Método de inserir novo drone.*/
+  public Drone inserirDrone(Drone drone) {
+    return repository.save(drone);
+  }
+
+}
