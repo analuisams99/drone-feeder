b5eb8dbccbbb3ad76ddf5b3670e8b07d3e19eb1e
Ana Luisa <ana.luisa.isa@hotmail.com>
2023-04-04 17:59:25 -0300

    feat(controller/drone): Implementação de DroneController, injetando a dependencia DroneService e definindo o caminho /drones

--- src/main/java/com/futuereh/dronefeeder/controller/DroneController.java
+++ src/main/java/com/futuereh/dronefeeder/controller/DroneController.java
@@ -1,6 +1,16 @@
 package com.futuereh.dronefeeder.controller;
 
+import com.futuereh.dronefeeder.service.DroneService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**Classe DroneController.*/
+@RestController
+@RequestMapping("/drones")
 public class DroneController {
+
+  @Autowired
+  private DroneService droneService;
 
 }
