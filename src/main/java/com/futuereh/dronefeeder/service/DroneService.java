9854b67a9eb064755bdf9b20abc7f5d938e84c50
Ana Luisa <ana.luisa.isa@hotmail.com>
2023-04-04 17:32:30 -0300

    feat(service): Implemetação do método opcional retornarDronePeloId

--- src/main/java/com/futuereh/dronefeeder/service/DroneService.java
+++ src/main/java/com/futuereh/dronefeeder/service/DroneService.java
@@ -3,6 +3,7 @@ package com.futuereh.dronefeeder.service;
 import com.futuereh.dronefeeder.model.Drone;
 import com.futuereh.dronefeeder.repository.DroneRepository;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,6 +21,11 @@ public class DroneService {
   /**Método de retornar todos os drones.*/
   public List<Drone> retornarTodosOsDrones() {
     return repository.findAll();
+  }
+
+  /**Método de retornar drone pelo id.*/
+  public Optional<Drone> retornarDronePeloId(Long id) {
+    return repository.findById(id);
   }
 
 }
