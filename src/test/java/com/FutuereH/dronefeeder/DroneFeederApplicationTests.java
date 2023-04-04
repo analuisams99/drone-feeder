a166a0ea073b7d1d9e999fe1db3193cfbc6571f0
Ana Luisa <ana.luisa.isa@hotmail.com>
2023-04-04 15:15:30 -0300

    Iniciando projeto final DroneFeeder

---
+++ src/test/java/com/FutuereH/dronefeeder/DroneFeederApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FutuereH.dronefeeder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneFeederApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
