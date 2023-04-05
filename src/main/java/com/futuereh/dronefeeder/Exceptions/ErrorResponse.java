a524fd0a74f556505620067a32e156952253bfd0
Pedro Pereira <pedro.2108@hotmail.com>
2023-04-04 21:17:27 -0300

    feat: excecoes nao encontrada e error response implementadas

---
+++ src/main/java/com/futuereh/dronefeeder/Exceptions/ErrorResponse.java
@@ -0,0 +1,13 @@
+package com.futuereh.dronefeeder.Exceptions;
+
+public class ErrorResponse {
+  private final String error;
+
+  public ErrorResponse(String error) {
+    this.error = error;
+  }
+
+  public String getError() {
+    return error;
+  }
+}
---
+++ src/main/java/com/futuereh/dronefeeder/Exceptions/NaoEncontradoException.java
@@ -0,0 +1,7 @@
+package com.futuereh.dronefeeder.Exceptions;
+
+public class NaoEncontradoException extends RuntimeException{
+  public NaoEncontradoException(String message) {
+    super(message);
+  }
+}
