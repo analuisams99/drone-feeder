20222deb8c6fd1afa2faf7945ea02f51627fab2c
Pedro Pereira <pedro.2108@hotmail.com>
2023-04-04 23:39:14 -0300

    feat: Controller para excpetions implementado

---
+++ src/main/java/com/futuereh/dronefeeder/controller/ExceptionController.java
@@ -0,0 +1,34 @@
+package com.futuereh.dronefeeder.controller;
+
+import com.futuereh.dronefeeder.exceptions.ErrorResponse;
+import com.futuereh.dronefeeder.exceptions.NaoEncontradoException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+/**
+ * Exception Controller.
+ */
+@ControllerAdvice
+public class ExceptionController {
+  /**
+   * Not Found Error.
+   *
+   * @param exception NaoEncontrada
+   * @return Mensagem de erro
+   */
+  @ExceptionHandler({NaoEncontradoException.class})
+  public ResponseEntity<ErrorResponse> notFound(RuntimeException exception) {
+    ErrorResponse response = new ErrorResponse(exception.getMessage());
+
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+  }
+
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<ErrorResponse> internalError(RuntimeException exception) {
+    ErrorResponse response = new ErrorResponse(exception.getMessage());
+
+    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+  }
+}
