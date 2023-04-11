97efc239eb5671fec47a5a97a5d29cae049bd7b4
Ana Luisa <ana.luisa.isa@hotmail.com>
2023-04-11 20:27:30 -0300

    fix(model/drone): Coloca Cascata na relação da entidade

--- src/main/java/com/futuereh/dronefeeder/model/Drone.java
+++ src/main/java/com/futuereh/dronefeeder/model/Drone.java
@@ -2,6 +2,7 @@ package com.futuereh.dronefeeder.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -25,7 +26,7 @@ public class Drone {
   private double longitudeAtual;
 
   @Column
-  @OneToMany(mappedBy = "drone")
+  @OneToMany(mappedBy = "drone", cascade = CascadeType.ALL)
   private List<Entrega> entregas = new ArrayList<>();
 
   public Drone() {}
